2a49f9fe730c3d6f8d8482be973cce047a93fd23
dam62 <vicentemcr16@gmail.com>
2026-03-07 01:12:09 +0100

    Juego arrastrar con un nivel

--- InteractiveApp/ViewModels/ArrastrarViewModel.cs
+++ InteractiveApp/ViewModels/ArrastrarViewModel.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Linq;
+using System.Threading.Tasks;
 using Avalonia;
+using Avalonia.Collections;
 using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
 using Avalonia.Input;
 using Avalonia.Media;
+using Avalonia.Threading;
 using Avalonia.VisualTree;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
@@ -15,41 +18,46 @@ public partial class ArrastrarViewModel : ViewModelBase
 {
     [ObservableProperty] private bool isLevelOk;
     
+    private Point _posI1, _posI2, _posI3, _posI4, _posI5, _posI6;
+    private int _correctos = 0;
+
     [RelayCommand]
     private void DragDelta(VectorEventArgs e)
     {
-        if (e.Source is not Thumb)
-        {
-            return;
-        }
+        if (e.Source is not Thumb thumb) return;
         
-        var thumb = (Thumb)e.Source;
         if (thumb.RenderTransform is not TranslateTransform t)
         {
             t = new TranslateTransform();
             thumb.RenderTransform = t;
         }
-        
+
+        switch (thumb.Name)
+        {
+            case "I1": if (_posI1 == default) _posI1 = new Point(t.X, t.Y); break;
+            case "I2": if (_posI2 == default) _posI2 = new Point(t.X, t.Y); break;
+            case "I3": if (_posI3 == default) _posI3 = new Point(t.X, t.Y); break;
+            case "I4": if (_posI4 == default) _posI4 = new Point(t.X, t.Y); break;
+            case "I5": if (_posI5 == default) _posI5 = new Point(t.X, t.Y); break;
+            case "I6": if (_posI6 == default) _posI6 = new Point(t.X, t.Y); break;
+        }
+
         t.X += e.Vector.X;
         t.Y += e.Vector.Y;
     }
-    
+
     [RelayCommand]
     public void DragCompleted(VectorEventArgs e)
     {
-        if (e.Source is not Thumb thumb)
-            return;
+        if (e.Source is not Thumb thumb) return;
 
         var window = TopLevel.GetTopLevel(thumb);
-
         var thumbRect = GetRect(thumb, window);
-        if (thumbRect == null)
-            return;
+        if (thumbRect == null) return;
 
         var view = thumb.FindAncestorOfType<UserControl>();
-        if (view == null)
-            return;
-
+        if (view == null) return;
+        
         var huecoV = view.FindControl<Border>("HuecoV");
         var huecoC = view.FindControl<Border>("HuecoC");
         var huecoP = view.FindControl<Border>("HuecoP");
@@ -57,52 +65,62 @@ public partial class ArrastrarViewModel : ViewModelBase
         var rectV = GetRect(huecoV, window);
         var rectC = GetRect(huecoC, window);
         var rectP = GetRect(huecoP, window);
+        
+        string categoria = (string)thumb.Tag;
+        
+        bool correcto = categoria switch
+        {
+            "HuecoV" => rectV?.Intersects(thumbRect.Value) == true,
+            "HuecoC" => rectC?.Intersects(thumbRect.Value) == true,
+            "HuecoP" => rectP?.Intersects(thumbRect.Value) == true,
+            _ => false
+        };
 
-        if (rectV != null && thumbRect.Value.Intersects(rectV.Value) && thumb.Name == "I1")
+        if (correcto)
         {
             thumb.IsVisible = false;
+            _correctos++;
+
+            if (_correctos == 6)
+                IsLevelOk = true;
         }
-        else if (rectC != null && thumbRect.Value.Intersects(rectC.Value) && thumb.Name == "I2")
+        else
         {
-            thumb.IsVisible = false;
-        }
-        else if (rectP != null && thumbRect.Value.Intersects(rectP.Value) && thumb.Name == "I3")
-        {
-            thumb.IsVisible = false;
-        }
-        else if (rectP != null && thumbRect.Value.Intersects(rectP.Value) && thumb.Name == "I4")
-        {
-            thumb.IsVisible = false;
-        }
-        else if (rectP != null && thumbRect.Value.Intersects(rectP.Value) && thumb.Name == "I5")
-        {
-            thumb.IsVisible = false;
-        }
-        else if (rectP != null && thumbRect.Value.Intersects(rectP.Value) && thumb.Name == "I6")
-        {
-            thumb.IsVisible = false;
+            VolverAPosicionInicial(thumb);
         }
     }
-    
+
+    private void VolverAPosicionInicial(Thumb thumb)
+    {
+        if (thumb.RenderTransform is not TranslateTransform t)
+        {
+            t = new TranslateTransform();
+            thumb.RenderTransform = t;
+        }
+
+        switch (thumb.Name)
+        {
+            case "I1": t.X = _posI1.X; t.Y = _posI1.Y; break;
+            case "I2": t.X = _posI2.X; t.Y = _posI2.Y; break;
+            case "I3": t.X = _posI3.X; t.Y = _posI3.Y; break;
+            case "I4": t.X = _posI4.X; t.Y = _posI4.Y; break;
+            case "I5": t.X = _posI5.X; t.Y = _posI5.Y; break;
+            case "I6": t.X = _posI6.X; t.Y = _posI6.Y; break;
+        }
+    }
+
     private Rect? GetRect(Control control, TopLevel window)
     {
-        var pos = control.TranslatePoint(new Point(0,0), window);
-        if (pos is null)
-            return null;
-
+        var pos = control.TranslatePoint(new Point(0, 0), window);
+        if (pos is null) return null;
         return new Rect(pos.Value, control.Bounds.Size);
     }
-    
+
     [RelayCommand]
     public void Scroll(VectorEventArgs e)
     {
-        if (IsLevelOk)
-        {
-            return;
-        }
-        
-        if (e.Source is not Thumb thumb)
-            return;
+        if (IsLevelOk) return;
+        if (e.Source is not Thumb thumb) return;
 
         if (thumb.RenderTransform is not TranslateTransform t)
         {
